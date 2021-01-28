3c40d838728cdfb82b4ecfe67cca03a9481fa521
XeleniumDev <ilyaoc@yandex.kz>
2021-01-28 21:02:51 +0200

    Remove delay parameter from screenshot endpoint

--- handlers.go
+++ handlers.go
@@ -2,32 +2,22 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/chromedp/chromedp"
-	"gopkg.in/labstack/echo.v4"
+	"github.com/labstack/echo"
 )
 
 // Basic screenshot impemtation
 func screenshot(c echo.Context) error {
-	var delay int = 3
 	url := c.QueryParam("url")
 	if url == "" {
 		return c.String(http.StatusNotAcceptable, `Required parameter "url" is empty.`)
 	}
-	if c.QueryParam("delay") != "" {
-		var err error
-		delay, err = strconv.Atoi(c.QueryParam("delay"))
-		if err != nil {
-			return c.String(http.StatusNotAcceptable, `Datatype for parameter "delay" is invalid.`)
-		}
-	}
 
 	var buf []byte
-	if err := chromedp.Run(ctx, fullScreenshot(url, 90, time.Duration(delay)*time.Second, &buf)); err != nil {
+	if err := chromedp.Run(ctx, fullScreenshot(url, 90, time.Duration(screenshotDelay)*time.Second, &buf)); err != nil {
 		return err
 	}
 	reader := bytes.NewReader(buf)
--- main.go
+++ main.go
@@ -2,24 +2,79 @@ package main
 
 import (
 	"context"
+	"os"
+	"strconv"
 
 	"github.com/chromedp/chromedp"
-	"gopkg.in/labstack/echo.v4"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"golang.org/x/time/rate"
 )
 
 var (
+	// Internal variables
 	ctx    context.Context
+	cx     context.Context
 	cancel context.CancelFunc
+	// User-defined variables
+	cacheDir        string = "./cache"
+	port            string = ":5000"
+	cooldown        int    = 2 // seconds
+	screenshotDelay int    = 2 // seconds
+	proxyServer     string
 )
 
 func main() {
 	e := echo.New()
 
-	ctx, cancel := chromedp.NewContext(context.Background())
+	Init()
+
+	if proxyServer != "" {
+
+		o := append(chromedp.DefaultExecAllocatorOptions[:],
+			chromedp.ProxyServer(proxyServer),
+		)
+		cx, cancel = chromedp.NewExecAllocator(context.Background(), o...)
+		defer cancel()
+	}
+
+	ctx, cancel = chromedp.NewContext(cx)
 	defer cancel()
 
-	e.Use(bindContext(ctx))
+	rateLimit(e)
+
 	e.GET("/", screenshot)
 
-	e.Logger.Fatal(e.Start(":5000"))
+	e.Logger.Fatal(
+		e.Start(port),
+	)
+}
+
+func rateLimit(e *echo.Echo) {
+	// Allow only 30 requests per second per IP (i.e. 1 request per 2 seconds )
+	var ratelimit int = 60 / cooldown
+	var limiterStore = middleware.NewRateLimiterMemoryStore(rate.Limit(ratelimit))
+
+	limiter := middleware.RateLimiter(limiterStore)
+
+	e.Use(limiter)
+}
+
+// Init configuration
+func Init() {
+	// TODO add non-cache mode
+	os.RemoveAll(cacheDir)
+	if _, err := os.Stat(cacheDir); os.IsNotExist(err) {
+		os.Mkdir(cacheDir, 0755)
+	}
+	if _, err := strconv.Atoi(os.Getenv("PORT")); err == nil {
+		port = ":" + os.Getenv("PORT")
+	}
+	if cd, err := strconv.Atoi(os.Getenv("COOLDOWN")); err == nil {
+		cooldown = cd
+	}
+	if dl, err := strconv.Atoi(os.Getenv("WAIT_BEFORE_SCREENSHOT")); err == nil {
+		screenshotDelay = dl
+	}
+	proxyServer = os.Getenv("PROXY")
 }
